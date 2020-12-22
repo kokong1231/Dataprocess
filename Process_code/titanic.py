ff6cb9e0ad9094ab3ad3bf0813ef163e9f812ee3
kokong <kokong1231@gmail.com>
2020-12-22 17:17:31 +0900

    Preprocessing the Titanic dataset
    Titanic data set missing values ​​removed and categorized
    
    by ohjihun

--- Process_code/titanic.py
+++ Process_code/titanic.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import numpy as np
 
+from matplotlib import pyplot as plt
 
-gender_data = pd.read_csv('../titanic/gender_submission.csv')
-test_data = pd.read_csv('../titanic/test.csv')
+
+import seaborn as sns
+
+'''
+seaborn : 예제 데이터 셋을 가져오는 모듈
+여러개의 예제 데이터 셋이 있음
+'''
+
 train_data = pd.read_csv('../titanic/train.csv')
+del_nan = train_data['Age'].dropna(axis=0)
 
 
-print(gender_data, test_data, train_data)
+bins = [0, 10, 20, 30, 40, 50, 60 ,70 ,80, 90]
+labels = ['10', '20', '30', '40', '50', '60', '70', '80', '90']
 
+age_categories = pd.cut(del_nan, bins, labels=labels)
+
+print(age_categories.conut())
