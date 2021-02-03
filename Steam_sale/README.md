8a411517e473b1c6934842f83ddd8ffa7b1b1dd5
kokong <kokong1231@gmail.com>
2021-02-04 01:06:59 +0900

    스팀 세일율 빈도 그래프 출력 완료

--- Steam_sale/README.md
+++ Steam_sale/README.md
@@ -5,3 +5,6 @@
 <img src="../Img/DLC.png" width="300px" height="250px">
     - 2020년 2월 3일 기준 약 세일 목록의 약 20%가 DLC가 차지하고 있음
 
+## 세일 범위 범주화 및 각 범위별 분포도
+<img src="../Img/Sale.png" width="300px" height="250px">
+    - 2020년 2월 3일 기준 스팀 할인율 빈도
--- Steam_sale/sale_rate.py
+++ Steam_sale/sale_rate.py
@@ -2,26 +2,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('./dataset/sample_data.csv')
+data = pd.read_csv('./dataset/steam_sale_list.csv')
 
-rate_data = pd.DataFrame(data['sale'].value_counts(normalize=True, sort=True).reset_index())
-rate_data.columns = ['sale', 'average']
-rate_data['average'] = pd.Series(["{0:.2f}%".format(val * 100) for val in rate_data['average']], index = rate_data.index)
-rate_data['sale'] = pd.Series([val * -1 for val in rate_data['sale']], index = rate_data.index)
+# rate_data = pd.DataFrame(data['sale'].value_counts(normalize=True, sort=True).reset_index())
+# data.columns = ['sale', 'average']
+# rate_data['average'] = pd.Series(["{0:.2f}%".format(val * 100) for val in rate_data['average']], index = rate_data.index)
+# rate_data['sale'] = pd.Series([val * -1 for val in rate_data['sale']], index = rate_data.index)
 
 
 
 range_value = [0,10,20,30,40,50,60,70,80,90,100]
-range_name = ['1%-10%', '11%-20%', '21%-30%', '31%-40%', '41%-50%', '51%-60%', '61%-70%', '71%-80%', '81%-90%', '91%-']
-rate_categories = pd.cut(rate_data['sale'], list(map(float, range_value)), labels = range_name)
-print(rate_data)
+range_name = ['0%-9%', '10%-19%', '20%-29%', '30%-39%', '40%-49%', '50%-59%', '60%-69%', '70%-79%', '80%-89%', '90%-99%']
+rate_categories = pd.cut(data['sale'], 10, labels = range_name)
+rate_df = pd.DataFrame(data=rate_categories)
+rate = rate_df['sale'].value_counts(normalize=False, sort=False)
 
 
 
-x = np.arange(67)
-sale_value = rate_data['sale']
-values = rate_data['average']
+x = np.arange(len(rate))
 
-plt.bar(x, values)
-plt.xticks(x, sale_value)
+plt.bar(x, rate)
+plt.xticks(x, range_name)
+
+for i, v in enumerate(x):
+    plt.text(v, rate[i], rate[i],                 # 좌표 (x축 = v, y축 = y[0]..y[1], 표시 = y[0]..y[1])
+             fontsize = 9, 
+             color='black',
+             horizontalalignment='center',  # horizontalalignment (left, center, right)
+             verticalalignment='bottom')    # verticalalignment (top, center, bottom)
+
 plt.show()
